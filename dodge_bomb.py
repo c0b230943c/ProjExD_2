e856d9ba081b4bbd8c847ed398677b477d9cbbb6
C0B23094 <c0b230943c@edu.teu.ac.jpgit cogit config --global user.email c0b230943c@edu.teu.ac.jp>
2024-06-18 14:48:13 +0900

    練習問題4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -43,6 +43,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct): #こうかとんと爆弾の衝突判定
+            return  # gameover
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()

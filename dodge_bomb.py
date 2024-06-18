b6a579031902ac7b0c2be141c06e232e5056ca2b
C0B23094 <c0b230943c@edu.teu.ac.jpgit cogit config --global user.email c0b230943c@edu.teu.ac.jp>
2024-06-18 17:06:04 +0900

    演習問題2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pygame as pg
 import random
+import time
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -24,7 +25,12 @@ def InOut(rct:pg.Rect) -> tuple[bool,bool]:
     return yoko,tate
 
 
-def Rotate(image):
+def Rotate(image:pg.Surface):
+    """
+    引数:回転させるSurface
+    戻り値:各キーの入力に応じて回転、反転したSurface
+    キー入力の合計値のタプルをキーとして、回転後のSurfaceをバリューとする辞書を作る
+    """
     dict = {(-5,0):pg.transform.rotozoom(image,0,1.0),
             (-5,5):pg.transform.rotozoom(image,45,1.0),
             (0,5):pg.transform.flip(pg.transform.rotozoom(image,90,1.0),True,False),
@@ -36,6 +42,22 @@ def Rotate(image):
     return dict
 
 
+def Harder():
+    """
+    引数:無し
+    戻り値:加速度と拡大した爆弾のSurfaceのリストを二つ合わせたタプル
+    加速度と拡大したSurfaceをそれぞれリストにしたものをタプルとして合体させてreturnした
+    """
+    accs = [a for a in range(1,11)]
+    bb_imgs = []
+    for r in range(1,11):
+        bb_img = pg.Surface((20*r,20*r))
+        pg.draw.circle(bb_img,(225,0,0),(10*r,10*r),10*r)
+        bb_img.set_colorkey((0,0,0))
+        bb_imgs.append(bb_img)
+    return accs, bb_imgs
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -44,14 +66,16 @@ def main():
     kk_dict = Rotate(kk_img)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
-    enn = pg.Surface((20,20))
-    pg.draw.circle(enn,(255,0,0),(10,10),10)
-    enn.set_colorkey((0,0,0))
-    bb_rct = enn.get_rect()
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_img.set_colorkey((0,0,0))
+    bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
     vx,vy = 5,5  # 爆弾の横、縦の移動速度
     clock = pg.time.Clock()
     tmr = 0
+    bb_accs = Harder()[0]
+    bb_imgs = Harder()[1]
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -60,6 +84,9 @@ def main():
             return  # gameover
         screen.blit(bg_img, [0, 0]) 
 
+        avx = vx*bb_accs[min(tmr//500,9)]
+        bb_img = bb_imgs[min(tmr//500,9)]
+    
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k,v in KEY_DICT.items():
@@ -67,19 +94,20 @@ def main():
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
+
         if InOut(kk_rct) != (True,True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         for k, v in kk_dict.items():
             if k == tuple(sum_mv):
                 kk_img = v
-            else:
-                screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx,vy)
+        screen.blit(kk_img, kk_rct)
+        
+        bb_rct.move_ip(avx,vy)
         if not InOut(bb_rct)[0]:
             vx = vx * -1
         if not InOut(bb_rct)[1]:
             vy = vy * -1
-        screen.blit(enn,bb_rct)
+        screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
